f5876d772c116e833c196632845ba2e7009ecb2c
Samaklen <laphatpun@gmail.com>
2024-12-14 16:26:08 +0700

    Initial commit for my project

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const userController = require('./controller/UserController');
+const productController = require('./controller/ProductController')
+const saleController = require('./controller/SaleController')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+app.use('/user', userController);
+app.use('/product', productController);
+app.use('/api/sale', saleController)
+app.listen(3001);
