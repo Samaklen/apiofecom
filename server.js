74e1055a01743a4c9bdbee764be3c2edf10e4d08
Samaklen <laphatpun@gmail.com>
2024-12-15 11:34:39 +0700

    Add CSP header to allow specific script from Vercel

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const userController = require('./controller/UserController');
-const productController = require('./controller/ProductController')
-const saleController = require('./controller/SaleController')
+const productController = require('./controller/ProductController');
+const saleController = require('./controller/SaleController');
+
+const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
+// Add CSP header
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', "default-src 'none'; script-src 'self' https://vercel.live/_next-live/feedback/feedback.js");
+  next();
+});
+
 app.use('/user', userController);
 app.use('/product', productController);
-app.use('/api/sale', saleController)
-app.listen(3001);
+app.use('/api/sale', saleController);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
